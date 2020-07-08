bb086d0a433ef0b4146c8ce1518d231c7bdca156
Luca <l.puccetti@gmail.com>
2020-07-08 19:23:35 +0200

    First successful test.

--- src/twist2rc.cpp
+++ src/twist2rc.cpp
@@ -2,57 +2,20 @@
 #include <geometry_msgs/Twist.h>
 #include "rosrccar_messages/RCControl.h"
 
-/*
-class SubscribeAndPublish
-{
-public:
-  SubscribeAndPublish()
-  {
-    ros::Subscriber sub_ = n_.subscribe("key_vel", 1, &SubscribeAndPublish::callback, this);
-    ros::Publisher pub_ = n_.advertise<rosrccar_messages::RCControl>("rc_output", 1);
-    ROS_INFO("Subscribed to key_vel and publishing to rc_output");
-  }
-
-  void callback(const geometry_msgs::Twist& tw_msg)
-  {
-    rosrccar_messages::RCControl rc_msg;
-    rc_msg.accelerator = tw_msg.linear.x; // usually 0.8
-    rc_msg.steering = tw_msg.angular.z;  // between -1 and 1
-    rc_msg.valid = true;
-    pub_.publish(rc_msg);
-    ROS_INFO("Relayed a message.");
-  }
-
-private:
-  ros::NodeHandle n_; 
-  ros::Publisher pub_;
-  ros::Subscriber sub_;
-
-};//End of class SubscribeAndPublish
-
-int main(int argc, char **argv)
-{
-  //Initiate ROS
-  ros::init(argc, argv, "twist2rc");
-
-  //Create an object of class SubscribeAndPublish that will take care of everything
-  SubscribeAndPublish SAPObject;
-
-  ros::spin();
-
-  return 0;
-}
-*/
-
 float acceleration;
 float steering;
-
+ros::Publisher pub_;
 
 void convertercallback(const geometry_msgs::Twist& tw_msg)
 {
   rosrccar_messages::RCControl rc_msg;
   acceleration = (float)tw_msg.linear.x; // usually 0.8
   steering = (float)tw_msg.angular.z;  // between -1 and 1
+//   rosrccar_messages::RCControl rc_msg;
+  rc_msg.accelerator = acceleration*0.3; // usually 0.8
+  rc_msg.steering = -steering;  // between -1 and 1
+  rc_msg.valid = true;
+  pub_.publish(rc_msg);
 }
 
 int main(int argc, char **argv)
@@ -60,10 +23,10 @@ int main(int argc, char **argv)
   //Initiate ROS
   ros::init(argc, argv, "twist2rc");
   ros::NodeHandle n_; 
-  ros::Subscriber sub_ = n_.subscribe("key_vel", 1, convertercallback);
-  ros::Publisher pub_ = n_.advertise<rosrccar_messages::RCControl>("rc_output", 1);
-  ROS_INFO("Subscribed to key_vel and publishing to rc_output");
-
+  ros::Subscriber sub_ = n_.subscribe("cmd_vel", 1, convertercallback);
+  pub_ = n_.advertise<rosrccar_messages::RCControl>("rc_output", 1);
+  ROS_INFO("Subscribed to cmd_vel and publishing to rc_output");
+/*
   ros::Rate loop_rate(100);
 
   int count = 0;
@@ -71,7 +34,7 @@ int main(int argc, char **argv)
   {
     
     rosrccar_messages::RCControl rc_msg;
-    rc_msg.accelerator = acceleration*0.125; // usually 0.8
+    rc_msg.accelerator = acceleration*0.25; // usually 0.8
     rc_msg.steering = steering;  // between -1 and 1
     rc_msg.valid = true;
     pub_.publish(rc_msg);
@@ -81,6 +44,7 @@ int main(int argc, char **argv)
     loop_rate.sleep();
     ++count;
   }
-
+*/
+  ros::spin();
   return 0;
 }
