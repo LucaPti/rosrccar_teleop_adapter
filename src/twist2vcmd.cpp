096d4b66d7664d7903854be6e628395e597a4702
Luca <l.puccetti@gmail.com>
2020-10-04 16:30:15 +0200

    New converter for vehiclecommand message

---
+++ src/twist2vcmd.cpp
@@ -0,0 +1,31 @@
+#include "ros/ros.h"
+#include <geometry_msgs/Twist.h>
+#include "rosrccar_messages/VehicleCommand.h"
+
+float acceleration;
+float steering;
+ros::Publisher pub_;
+
+void convertercallback(const geometry_msgs::Twist& tw_msg)
+{
+  rosrccar_messages::VehicleCommand vcmd_msg;
+  acceleration = (float)tw_msg.linear.x; // usually 0.8
+  steering = (float)tw_msg.angular.z;  // between -1 and 1
+  vcmd_msg.target_lon = acceleration*1e3; // usually 0.8
+  vcmd_msg.target_lat = -steering*1e3;  // between -1 and 1
+  vcmd_msg.operationmode_lon = 4; //"automated" mode
+  vcmd_msg.operationmode_lat = 4; //"automated" mode
+  pub_.publish(vcmd_msg);
+}
+
+int main(int argc, char **argv)
+{
+  //Initiate ROS
+  ros::init(argc, argv, "twist2vcmd");
+  ros::NodeHandle n_; 
+  ros::Subscriber sub_ = n_.subscribe("cmd_vel", 1, convertercallback);
+  pub_ = n_.advertise<rosrccar_messages::VehicleCommand>("vehicle_command", 1);
+  ROS_INFO("Subscribed to cmd_vel and publishing to vehicle_command");
+  ros::spin();
+  return 0;
+}
